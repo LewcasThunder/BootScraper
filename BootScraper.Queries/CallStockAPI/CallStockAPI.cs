ee769790ab3180a859c4f4cc3c3e6295fc1c8f09
Lewcas Thunder <lewcasthunder@gmail.com>
2023-12-11 13:54:34 +0000

    Fix issue where API returns in stock data for other dosages of same medicine

--- BootScraper.Queries/CallStockAPI/CallStockAPI.cs
+++ BootScraper.Queries/CallStockAPI/CallStockAPI.cs
@@ -18,11 +18,12 @@ namespace BootScraper.Queries.CallStockAPI
 
             var serializeObject = JsonConvert.SerializeObject(requestData);
             request.Content = new StringContent(serializeObject, null, "application/json");
-            var response = client.SendAsync(request).Result;
-            response.EnsureSuccessStatusCode();
-            var responseString = response.Content.ReadAsStringAsync().Result;
 
-            return JsonConvert.DeserializeObject<StockResponseModel>(responseString);
+            var response = client.SendAsync(request).Result;
+            response.EnsureSuccessStatusCode(); var responseString = response.Content.ReadAsStringAsync().Result;
+            var stockResponseModel = JsonConvert.DeserializeObject<StockResponseModel>(responseString);
+
+            return new StockResponseModel{ stockLevels = stockResponseModel.stockLevels.Where(stock => stock.productId == productId).ToArray()};
         }
     }
 }
